9390b6144d8e9103136942f8fa43ce68a351b4ec
jayteen <62333772+jayteen@users.noreply.github.com>
2023-07-28 11:09:26 +0800

    test datePicker

--- demo.tsx
+++ demo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './index.css';
 import type { DatePickerProps } from 'antd';
 import { DatePicker, Space } from 'antd';
@@ -12,6 +12,7 @@ const { RangePicker } = DatePicker;
 const dateFormat = 'YYYY/MM/DD';
 const weekFormat = 'MM/DD';
 const monthFormat = 'YYYY/MM';
+const yearFormat = 'YYYY';
 
 /** Manually entering any of the following formats will perform date parsing */
 const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
@@ -24,18 +25,60 @@ const customWeekStartEndFormat: DatePickerProps['format'] = (value) =>
     .endOf('week')
     .format(weekFormat)}`;
 
-const App: React.FC = () => (
-  <Space direction="vertical" size={12}>
-    <DatePicker defaultValue={dayjs('2015/01/01', dateFormat)} format={dateFormat} />
-    <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} />
-    <DatePicker defaultValue={dayjs('2015/01', monthFormat)} format={monthFormat} picker="month" />
-    <DatePicker defaultValue={dayjs()} format={customWeekStartEndFormat} picker="week" />
-    <RangePicker
-      defaultValue={[dayjs('2015/01/01', dateFormat), dayjs('2015/01/01', dateFormat)]}
-      format={dateFormat}
-    />
-    <DatePicker defaultValue={dayjs('2015/01/01', dateFormat)} format={customFormat} />
-  </Space>
-);
+const App: React.FC = () => {
+  const [date, setDate] = useState('2023');
+
+  const handleDateChange = useCallback(
+    (value) => {
+      console.log(dayjs('2023'));
+      console.log(value);
+      const aaa = dayjs(value);
+      console.log(aaa);
+      setDate(value);
+    },
+    [setDate]
+  );
+
+  return (
+    <Space direction="vertical" size={12}>
+      <DatePicker
+        defaultValue={dayjs('2015/01/01', dateFormat)}
+        format={dateFormat}
+      />
+      <DatePicker
+        defaultValue={dayjs('01/01/2015', dateFormatList[0])}
+        format={dateFormatList}
+      />
+      <DatePicker
+        defaultValue={dayjs('2015/01', monthFormat)}
+        format={monthFormat}
+        picker="month"
+      />
+      测试年份：
+      <DatePicker
+        value={date ? dayjs(date, yearFormat) : null}
+        format={yearFormat}
+        picker="year"
+        onChange={handleDateChange}
+      />
+      <DatePicker
+        defaultValue={dayjs()}
+        format={customWeekStartEndFormat}
+        picker="week"
+      />
+      <RangePicker
+        defaultValue={[
+          dayjs('2015/01/01', dateFormat),
+          dayjs('2015/01/01', dateFormat),
+        ]}
+        format={dateFormat}
+      />
+      <DatePicker
+        defaultValue={dayjs('2015/01/01', dateFormat)}
+        format={customFormat}
+      />
+    </Space>
+  );
+};
 
 export default App;
